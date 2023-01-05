85eb38de1686ba613718802e1039009727eea238
Takahiro H <43534055+takahiro-hanada@users.noreply.github.com>
2023-01-05 17:49:57 +0900

    Internal メンバを公開

--- src/Storex.Core/SymbolLocation.cs
+++ src/Storex.Core/SymbolLocation.cs
@@ -1,48 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Storex
 {
-    internal sealed class SymbolLocation : IEquatable<SymbolLocation>
+    /// <summary>
+    /// シンボルのロケーションを表します。
+    /// </summary>
+    public sealed class SymbolLocation
     {
+        /// <summary>
+        /// 頂点座標。
+        /// </summary>
         public IReadOnlyList<PointD> VertexPoints;
 
         /// <summary>
-        /// 文字の読める向きを0度とし反時計回りの角度とする
+        /// 反時計回りの回転角度。
         /// </summary>
         public double ValueAngle { get; }
 
+        /// <summary>
+        /// <see cref="SymbolLocation"/> クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="vertexPoints"></param>
+        /// <param name="valueAngle"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
         public SymbolLocation(PointD[] vertexPoints, double valueAngle)
         {
-            //四角形のみ対象
-            if (vertexPoints.Length != 4)
-                throw new InvalidOperationException();
+            if (vertexPoints is null)
+                throw new ArgumentNullException(nameof(vertexPoints));
+
+            if (vertexPoints.Length != 2 & vertexPoints.Length != 4)
+                throw new ArgumentOutOfRangeException(nameof(vertexPoints));
 
             VertexPoints = Array.AsReadOnly(vertexPoints);
 
             ValueAngle = valueAngle;
         }
-
-        #region 等値演算
-
-        public override bool Equals(object obj) => Equals(obj as SymbolLocation);
-
-        public bool Equals(SymbolLocation other) => !(other is null)
-            && ((VertexPoints is null) & (other.VertexPoints is null) || Enumerable.SequenceEqual(VertexPoints, other.VertexPoints))
-            && ValueAngle == other.ValueAngle;
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                var hashCode = -1844779044;
-                hashCode = hashCode * -1521134295 + VertexPoints?.Where(e => e != null).Select(e => e.GetHashCode()).Aggregate(17, (a, b) => 23 * a + b) ?? 0;
-                hashCode = hashCode * -1521134295 + ValueAngle.GetHashCode();
-                return hashCode;
-            }
-        }
-
-        #endregion
     }
 }
